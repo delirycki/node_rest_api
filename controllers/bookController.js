ddaea6ef6dc2adc2674c992cdc8fdb1e71a53ca2
Paweł Komarnicki <delirycki@gmail.com>
2022-10-29 11:05:37 +0200

    add controlers

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,3 +1,24 @@
+const Book = require('../models/book');
+
+exports.index = (req, res) => {
+  res.send('NOT IMPLEMENTED: Site Home Page');
+};
+
+// Display list of all books.
+exports.book_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book list');
+};
+
+// Display detail page for a specific book.
+exports.book_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+};
+
+// Display book create form on GET.
+exports.book_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book create GET');
+};
+
 // Handle book create on POST.
 exports.book_create_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Book create POST');
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 const app = express();
 
@@ -9,6 +12,10 @@ app.listen(3000, () => {
   console.log(`Server Started at ${3000}`);
 });
 require('dotenv').config();
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
 
 const mongoString = process.env.URL_MONGO;
 
